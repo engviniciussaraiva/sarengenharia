8a1999b40fdc22e4ca4403ee978cdf85646f34c4
engviniciussaraiva <167394502+engviniciussaraiva@users.noreply.github.com>
2026-06-26 22:11:17 -0300

    Add comments and abrirPDFTecnico function
    Added header comments and initial PDF opening function.

--- projetotecnico/sistemashidraulicos/js/impressaoTecnica.js
+++ projetotecnico/sistemashidraulicos/js/impressaoTecnica.js
@@ -1,1 +1,29 @@
+/*
+=========================================================
+SAR ENGENHARIA
+Sistema Avançado de Resposta
 
+Motor de Impressão Técnica
+
+Arquivo:
+impressaoTecnica.js
+
+=========================================================
+*/
+
+
+
+/* =======================================================
+   ABRIR PDF TÉCNICO
+======================================================= */
+
+function abrirPDFTecnico(indice){
+
+    const l = linhas[indice];
+
+    if(!l){
+        alert("Registro não encontrado.");
+        return;
+    }
+
+}
